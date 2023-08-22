a36274e99f873f82ef213cc4c8794b925866ff93
Duygu <iamduygugenc@gmail.com>
2023-08-22 13:33:02 +0000

    customer dim model passed the scd type 1 test checks

--- models/mart/dwh/customer/dim_customer.sql
+++ models/mart/dwh/customer/dim_customer.sql
@@ -1,3 +1,12 @@
+
+{{
+    config(
+        materialized='incremental',
+        unique_key = 'customer_key',
+        incremental_strategy='merge',
+        merge_update_columns = ['last_name','marital_status','children_count','phone_number','changedate']
+    )
+}}
 with customer as(
     select * from {{ref('stg_tastybytes_customer')}}
 )
@@ -5,14 +14,15 @@ with customer as(
 select 
     customer_key,
     first_name,
-    {% if table_exists(this.database,this.schema,this.table) == 1 %}
-        CASE 
-            WHEN t2.customer_key is not null THEN cte.last_name
-            ELSE t2.last_name
-        end as last_name,
-    {% else %}
-        last_name,
-  {% endif %}
+    last_name,
+    --{% if table_exists(this.database,this.schema,this.table) == 1 %}
+    --    CASE 
+    --        WHEN t2.customer_key is not null THEN cte.last_name
+    --        ELSE t2.last_name
+    --    end as last_name,
+    --{% else %}
+    --    last_name,
+    --{% endif %}
     city,
     country,
     postal_code,
@@ -23,10 +33,9 @@ select
     register_date,
     birthday_date,
     email,
-    phone_number
+    phone_number,
+    current_timestamp() as changedate
 
 from customer
-{% if table_exists(this.database,this.schema,this.table) == 1 %}
-    LEFT JOIN {{this.database}}.{{this.schema}}.{{this.table}} as t2
-    ON customer.customer_key = t2.customer_key
-{% endif %}
+
+
